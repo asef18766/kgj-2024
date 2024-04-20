d7e00aeee643c8ad37afdb9f72693d83543424c0
bogay <pojay11523@gmail.com>
2024-04-20 16:17:46 +0800

    feat: impl Manager navigation

---
+++ Assets/GameAssets/Scripts/ManagerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "KGJ2024/ManagerData")]
+public class ManagerData : ScriptableObject
+{
+    public float speed;
+}
