bba450abb88e2039c0791b2de51cbe70f5cff857
unknown <asef18766@gmail.com>
2024-04-20 14:19:42 +0800

    add basic func in QTE

---
+++ Assets/Scripts/QTE/QTEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QTE
+{
+    public class QTEController:MonoBehaviour
+    {
+        [SerializeField] private Dictionary<KeyCode, Sprite> keyUI;
+
+        private void OnEnable()
+        {
+            throw new NotImplementedException();
+            //Input.GetKeyDown(())
+        }
+    }
+}
