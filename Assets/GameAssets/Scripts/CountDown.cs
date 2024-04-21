931ee19a20296ac3fb33e160195ee2ba84dd1005
unknown <asef18766@gmail.com>
2024-04-21 15:18:03 +0800

    possible production

---
+++ Assets/GameAssets/Scripts/CountDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameAssets.Scripts
+{
+    public class CountDown : MonoBehaviour
+    {
+        public int remainTime = 0;
+        [SerializeField] private int roundTime = 60;
+        private IEnumerator countDown()
+        {
+            while (remainTime > 0)
+            {
+                yield return new WaitForSeconds(1);
+                remainTime -= 1;
+            }
+            UnityEngine.SceneManagement.SceneManager.LoadScene("End");
+        }
+
+        private void Start()
+        {
+            remainTime = roundTime;
+            StartCoroutine(countDown());
+        }
+    }
+}
---
+++ Assets/GameAssets/Scripts/EndController.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameAssets.Scripts
+{
+    public class EndController : MonoBehaviour
+    {
+        [SerializeField] private GameDataScriptableObject userdata;
+        [SerializeField] private EndGameConfig cfg;
+        [SerializeField] private GameObject scoreboard;
+        [SerializeField] private GameObject returnMenu;
+        [SerializeField] private TMP_Text text;
+        private static readonly int EndNum = Animator.StringToHash("endNum");
+
+        private void Start()
+        {
+            GetComponent<Animator>().SetInteger(EndNum, userdata.score >= cfg.goodEndVal ? 1 : 2);
+        }
+
+        public void DisplayScore()
+        {
+            if (userdata.score >= cfg.goodEndVal)
+            {
+                returnMenu.SetActive(true);
+            }
+            else
+            {
+                scoreboard.SetActive(true);
+                text.text = $"{userdata.score}$";
+            }
+            GetComponent<AudioSource>().Play();
+
+        }
+    }
+}
